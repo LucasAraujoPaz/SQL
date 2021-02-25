463fa17193079caee9b8c63b0519c755b8f708e2
Lucas Paz <76001266+LucasAraujoPaz@users.noreply.github.com>
2021-02-25 19:42:33 -0300

    Update Leetcode hardest database problem.sql

--- Leetcode hardest database problem.sql
+++ Leetcode hardest database problem.sql
@@ -124,26 +124,26 @@ MySQL solution:
 */
 
 WITH 
-Unbanned_Requests as
-(SELECT Trips.Status, Trips.Request_at as `Day` 
-FROM Trips JOIN Users as U1 JOIN Users as U2
+Unbanned_Requests AS
+(SELECT Trips.Status, Trips.Request_at AS `Day` 
+FROM Trips JOIN Users AS U1 JOIN Users AS U2
 ON Trips.Client_Id = U1.Users_Id AND Trips.Driver_Id = U2.Users_Id
 WHERE U1.Banned = 'No' AND U2.Banned = 'No' AND 
 Trips.Request_at BETWEEN '2013-10-01' AND '2013-10-03'),
 
-Totals as 
-(SELECT `Day`, COUNT(*) as total_per_day FROM Unbanned_Requests
+Totals AS 
+(SELECT `Day`, COUNT(*) AS total_per_day FROM Unbanned_Requests
 GROUP BY `Day`),
 
-Cancelled as 
-(SELECT `Day`, COUNT(*) cancelled_per_day FROM Unbanned_Requests
+Cancelled AS 
+(SELECT `Day`, COUNT(*) AS cancelled_per_day FROM Unbanned_Requests
 WHERE `Status` <> 'completed'
 GROUP BY `Day`)
 
 SELECT 
 Totals.Day, 
 IF(cancelled_per_day IS NOT NULL, ROUND(cancelled_per_day/total_per_day, 2), 0.00) 
-as `Cancellation Rate`
+AS `Cancellation Rate`
 FROM Totals LEFT JOIN Cancelled ON Totals.Day = Cancelled.Day
 ;
 
